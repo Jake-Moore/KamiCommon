c6bfbf0726b0cd1fdbebe8be58aa6fb7df68ea27
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-03 22:29:08 -0800

    v1.0.28 - More translations

--- src/main/java/com/kamikazejamplugins/kamicommon/util/messageclicks/clicks/Click.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/util/messageclicks/clicks/Click.java
@@ -1,5 +1,6 @@
 package com.kamikazejamplugins.kamicommon.util.messageclicks.clicks;
 
+import com.kamikazejamplugins.kamicommon.util.StringUtil;
 import net.md_5.bungee.api.chat.TextComponent;
 
 public abstract class Click {
@@ -9,8 +10,8 @@ public abstract class Click {
 
     public Click(String replacement, String text, String hover) {
         this.replacement = replacement;
-        this.text = text;
-        this.hover = hover;
+        this.text = StringUtil.t(text);
+        this.hover = StringUtil.t(hover);
     }
 
     public abstract void setClickEvent(TextComponent component);
