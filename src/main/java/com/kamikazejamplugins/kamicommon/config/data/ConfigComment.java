f6a5081cf3f5fa7fd02c2d8b262b8b2f1980a3f6
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-05 15:02:55 -0800

    v1.3.1 - Allow the addAboveComment to accept multiple strings

--- src/main/java/com/kamikazejamplugins/kamicommon/config/data/ConfigComment.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/config/data/ConfigComment.java
@@ -2,15 +2,17 @@ package com.kamikazejamplugins.kamicommon.config.data;
 
 import lombok.Getter;
 
+import java.util.List;
+
 public class ConfigComment {
     @Getter private final String key;
-    @Getter private final String comment;
+    @Getter private final List<String> comment;
     @Getter private final boolean above;
 
     /**
      * Internal Class for the Parser, do not use :)
      */
-    public ConfigComment(String key, String comment, boolean above) {
+    public ConfigComment(String key, List<String> comment, boolean above) {
         this.key = key;
         this.comment = comment;
         this.above = above;
