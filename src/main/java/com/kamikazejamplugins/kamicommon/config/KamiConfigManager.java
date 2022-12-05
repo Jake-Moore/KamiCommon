61e02e804b8c39e387611ba112a440680fd8ee5c
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-05 14:38:00 -0800

    v1.3.0 - Rework the Config system to be so much smarter, so incredibly simple.

--- src/main/java/com/kamikazejamplugins/kamicommon/config/KamiConfigManager.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/config/KamiConfigManager.java
@@ -1,166 +1,35 @@
 package com.kamikazejamplugins.kamicommon.config;
 
-import com.kamikazejamplugins.kamicommon.config.annotation.ConfigValue;
 import com.kamikazejamplugins.kamicommon.config.data.ConfigComment;
 import com.kamikazejamplugins.kamicommon.config.data.KamiConfig;
 import com.kamikazejamplugins.kamicommon.util.StringUtil;
 import com.kamikazejamplugins.kamicommon.yaml.YamlHandler;
 
-import java.io.File;
-import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @SuppressWarnings("unused")
 public class KamiConfigManager {
 
-    /**
-     * Loads a config .yml file into a KamiConfig
-     * @param kamiConfig The KamiConfig to load its file into
-     */
-    public static YamlHandler.YamlConfiguration loadKamiConfigFromFile(KamiConfig kamiConfig) {
-        File file = kamiConfig.getFile();
-        YamlHandler yamlHandler = new YamlHandler(file);
-        YamlHandler.YamlConfiguration config = yamlHandler.loadConfig(false);
+    public static void saveKamiConfig(KamiConfig kamiConfig) throws Exception {
+        YamlHandler.YamlConfiguration config = kamiConfig.getYamlConfiguration();
+        List<ConfigComment> comments = kamiConfig.getComments();
 
-        // Key, FieldName
-        Map<String, String> fieldMappings = new HashMap<>();
-
-        // Fill the Field mappings
-        for (Field field : kamiConfig.getClass().getDeclaredFields()) {
-            ConfigValue annotation;
-
-            field.setAccessible(true);
-            if (!field.isAnnotationPresent(ConfigValue.class)) { continue; }
-            annotation = field.getAnnotation(ConfigValue.class);
-
-            // Define the fieldName (split by _ and take only the last argument)
-            String fieldName = field.getName().split("_")[field.getName().split("_").length-1];
-
-            // Build the key from the path and the field Name
-            String key = (annotation.path().isEmpty()) ? fieldName : annotation.path() + "." + fieldName;
-
-            // Store it
-            fieldMappings.put(key, field.getName());
-        }
-
-        // Loop through all the keys in the config, and update the KamiConfig variables
-        for (String key : config.getKeys(true)) {
-            try {
-                String fieldName = fieldMappings.getOrDefault(key, null);
-                if (fieldName == null) { continue; }
-                Field field = kamiConfig.getClass().getDeclaredField(fieldName);
-
-                field.setAccessible(true);
-                field.set(kamiConfig, config.get(key));
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return config;
-    }
-
-    /**
-     * Saves KamiConfig(s) to their file(s)
-     * @param kamiConfig The KamiConfig to save
-     */
-    public static void saveKamiConfigToFile(KamiConfig kamiConfig) {
-        try {
-            saveInternal(kamiConfig);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void saveInternal(KamiConfig kamiConfig) throws Exception {
-        YamlHandler yamlHandler = new YamlHandler(kamiConfig.getFile());
-        YamlHandler.YamlConfiguration config = yamlHandler.loadConfig(false);
-
-        List<ConfigComment> comments = new ArrayList<>();
-
-        // Loop through all the fields in the KamiConfig
-        for (Field field : kamiConfig.getClass().getDeclaredFields()) {
-            ConfigValue annotation;
-
-            field.setAccessible(true);
-            if (!field.isAnnotationPresent(ConfigValue.class)) { continue; }
-            annotation = field.getAnnotation(ConfigValue.class);
-
-            // Define the fieldName (split by _ and take only the last argument)
-            String fieldName = field.getName().split("_")[field.getName().split("_").length-1];
-
-            // Build the key from the path and the field Name
-            String key = (annotation.path().isEmpty()) ? fieldName : annotation.path() + "." + fieldName;
-
-            // Set the config values to the current KamiConfig value
-            config.set(key, field.get(kamiConfig));
-
-            // Add to the comments list for post processing
-            if (annotation.above().length != 0) {
-                int i = 0;
-                String[] parts = key.split("\\.");
-
-                for (String c : annotation.above()) {
-                    if (!c.isEmpty()) {
-                        String subKey = StringUtil.combine(StringUtil.subList(parts, 0, parts.length-i), ".");
-                        comments.add(new ConfigComment(subKey, c, true));
-                    }
-                    i++;
-                }
-            }
-            if (!annotation.inline().isEmpty()) {
-                String c = annotation.inline();
-                c = c.replace("\n", " ");
-                comments.add(new ConfigComment(key, c, false));
-            }
-        }
         // Save the FileConfiguration (without comments)
         config.save();
 
         // Store the lines here so that we don't have to read the file multiple times
         List<String> lines = Files.readAllLines(kamiConfig.getFile().toPath(), StandardCharsets.UTF_8);
 
-        // Add nice spacing
-        //saveWithSpaces(lines, kamiConfig);
-
         // Add the comments to the file
         for (ConfigComment comment : comments) {
             addComment(lines, comment);
         }
 
+        // Save the modified lines to the file
         Files.write(kamiConfig.getFile().toPath(), lines, StandardCharsets.UTF_8);
     }
-
-//    private static void saveWithSpaces(List<String> lines, KamiConfig kamiConfig) {
-//        // For every line that doesn't start with a space, or a comment, add a newline before it
-//        for (int i = 0; i < lines.size(); i++) {
-//            String line = lines.get(i);
-//            String starts = line.split(" ")[0];
-//            Pattern pattern = Pattern.compile("([\\w']+):");
-//
-//            Matcher matcher = pattern.matcher(starts);
-//            if (i > 0 && matcher.find()) {
-//                String key = starts.replace(":", "");
-//
-//                // Add a newLine above the key if there is going to also be a comment
-//                // Does not support depth of keys, ex "key1.key2.key3"
-//                //    (key3 has annotation, but key1 won't be spaced)
-//                try {
-//                    Field field = kamiConfig.getClass().getDeclaredField(key);
-//                    field.setAccessible(true);
-//                    ConfigValue annotation = field.getAnnotation(ConfigValue.class);
-//                    if (annotation.above().length != 0) {
-//                        lines.add(i, "");
-//                        i++;
-//                    }
-//                } catch (Exception ignored) {}
-//            }
-//        }
-//    }
 
     private static void addComment(List<String> lines, ConfigComment comment) {
         String[] parts = comment.getKey().split("\\.");
@@ -186,9 +55,6 @@ public class KamiConfigManager {
                                 lines.add(i, spacing + "# " + commentLine);
                             }
                         }
-
-                        //String c = comment.getComment().replace("\n", "\n" + spacing + "# ");
-                        //lines.add(i, spacing + "# " + c);
                     }else {
                         lines.set(i, line + " # " + comment.getComment());
                     }
--- src/main/java/com/kamikazejamplugins/kamicommon/config/testing/Config.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/config/testing/Config.java
@@ -1,50 +1,24 @@
 package com.kamikazejamplugins.kamicommon.config.testing;
 
-import com.kamikazejamplugins.kamicommon.config.annotation.ConfigValue;
-import com.kamikazejamplugins.kamicommon.config.data.KamiConfig;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Getter @Setter
-public class Config extends KamiConfig {
-
-    @ConfigValue(above = "The following settings are help messages.\nThe first is for normal players, the second for admins.\n{prefix} is replaced with cmdHelpPrefix.")
-    public List<String> cmdHelp = new ArrayList<>(Arrays.asList(
-            "&6_____________.[ &2FriendlyRaid Help &6]._______________",
-            "{prefix} raid &3<faction> &eInvite a faction to a friendly raid.",
-            "{prefix} forfeit &eLeave a friendly raid. (You cannot rejoin)",
-            "{prefix} counter &3confirm &eConfirm a counter schematic from ( %uploadsUrl% )",
-            "{prefix} counter &3edit &eRemove counter schematics from your profile.",
-            "{prefix} setspawn &eSet your faction's spawn point in a friendly raid.",
-            "{prefix} list &eView active Friendly Raids."
-    ));
-
-    @ConfigValue()
-    public List<String> cmdHelpManage = new ArrayList<>(Arrays.asList(
-            "&6_____________.[ &2FriendlyRaid Help &6]._______________",
-            "{prefix} raid &3<faction> &eInvite a faction to a friendly raid.",
-            "{prefix} forfeit &eLeave a friendly raid. (You cannot rejoin)",
-            "{prefix} counter &3confirm &eConfirm a counter schematic from ( %uploadsUrl% )",
-            "{prefix} counter &3edit &eRemove counter schematics from your profile.",
-            "{prefix} setspawn &eSet your faction's spawn point in a friendly raid.",
-            "{prefix} list &eView active Friendly Raids.",
-            "{prefix} reload &eReload the plugin config.yml",
-            "{prefix} freeitems <set|add|view> &eManage free items given in a Friendly Raid.",
-            "{prefix} tournament <start|stop|view> &eManage tournament mode (will start immediately)."
-    ));
-
-    @ConfigValue(above = "This is the format for the {prefix} above.\n{stem} is the command stem, e.g. /fraid.\nYou can add colors to the prefix here.")
-    public String cmdHelpPrefix = "&b{stem}";
-
-    @ConfigValue(path = "cmdUsages", above = {"", "\nThe following messages are sent when a player\n uses a command incorrectly."})
-    public String counterConfirm = "{prefix} counter &3(confirm | edit)";
-
-    public static void main(String[] args) throws Exception {
-        KamiConfig.create(Config.class, new File("C:\\Users\\Jake\\Desktop\\test.yml"));
-    }
-}
+//import com.kamikazejamplugins.kamicommon.config.data.KamiConfig;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.io.File;
+//
+//@Getter @Setter
+//public class Config extends KamiConfig {
+//
+//    public Config(File file) {
+//        super(file);
+//
+//        addCommentAbove("friendlyRaid", "This is a comment above the key");
+//        addCommentAbove("friendlyRaid.blacklistedCommands", "Test comment above");
+//
+//        addCommentAbove("friendlyRaid.blacklistedCommands", "Test");
+//    }
+//
+//    public static void main(String[] args) {
+//        Config config = new Config(new File("C:\\Users\\Jake\\Desktop\\config.yml"));
+//    }
+//}
