0a74c111bdbf74eb8032e568f27af773eb812115
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-04 20:46:12 -0800

    v1.2.3 - Field names will split by "_" and only take the last argument. So that config options can have specific names, but code can refer to them with prefixes.

--- src/main/java/com/kamikazejamplugins/kamicommon/config/testing/Config.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/config/testing/Config.java
@@ -2,18 +2,28 @@ package com.kamikazejamplugins.kamicommon.config.testing;
 
 //import com.kamikazejamplugins.kamicommon.config.annotation.ConfigValue;
 //import com.kamikazejamplugins.kamicommon.config.data.KamiConfig;
+//import lombok.Getter;
+//import lombok.Setter;
 //
 //import java.io.File;
-
+//
+//@Getter @Setter
 //public class Config extends KamiConfig {
 //
 //    @ConfigValue(path = "settings", above = {"Test comment"})
 //    public String testString = "test";
 //
-//    @ConfigValue(path = "settings2.options", above = {"Test comment1", "Test Comment2", "Settings Comment"})
-//    public String testString2 = "test2";
+//    @ConfigValue(path = "settings2.options")
+//    public int options_xi = 0;
+//
+//    @ConfigValue(path = "settings2.options")
+//    public int options_y = 0;
+//
+//    @ConfigValue(path = "settings2.options")
+//    public int options_z = 0;
 //
 //    public static void main(String[] args) throws Exception {
 //        Config config = KamiConfig.create(Config.class, new File("C:\\Users\\Jake\\Desktop\\test.yml"));
+//        config.getOptions_xi();
 //    }
 //}
