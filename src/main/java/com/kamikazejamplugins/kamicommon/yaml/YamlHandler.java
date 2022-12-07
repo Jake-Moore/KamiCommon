6eeca9a6af1fe6a5351d842978c51a16043a1f93
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-06 20:10:09 -0800

    v1.3.4.1 - YamlHandler add defaults maintains the same order as in the resource file.

--- src/main/java/com/kamikazejamplugins/kamicommon/yaml/YamlHandler.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/yaml/YamlHandler.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.annotation.Nullable;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.LinkedHashMap;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public class YamlHandler {
@@ -67,12 +67,7 @@ public class YamlHandler {
     }
 
     private YamlConfiguration addDefaults(YamlConfiguration config) {
-        InputStream defConfigStream;
-        if (plugin != null) {
-            defConfigStream = plugin.getResource(configFile.getName());
-        }else {
-            defConfigStream = getClass().getClassLoader().getResourceAsStream(File.separator + configFile.getName());
-        }
+        InputStream defConfigStream = getIS(plugin);
 
         if (defConfigStream == null) {
             System.out.println(ANSI.RED
@@ -81,14 +76,125 @@ public class YamlHandler {
             save();
             return config;
         }
-        MemoryConfiguration defConfig = new MemoryConfiguration((new Yaml()).load(defConfigStream));
-        for (String key : defConfig.getKeys(true)) {
+
+        List<String> keys = getInputStreamKeys(getIS(plugin));
+        LinkedHashMap<String, Object> data = (new Yaml()).load(defConfigStream);
+        MemoryConfiguration defConfig = new MemoryConfiguration(data);
+
+        if (!equalLists(keys, defConfig.getKeys(true))) {
+            System.out.println(ANSI.RED
+                    + "Warning: Error grabbing ordered defaults from (" + configFile.getName() + ")!"
+                    + ANSI.RESET);
+            save();
+            return config;
+        }
+
+        for (String key : keys) {
             if (!config.contains(key)) {
                 config.set(key, defConfig.get(key));
             }
         }
         save();
         return config;
+    }
+
+    private InputStream getIS(@Nullable JavaPlugin plugin) {
+        if (plugin != null) {
+            return plugin.getResource(configFile.getName());
+        }else {
+            return getClass().getClassLoader().getResourceAsStream(File.separator + configFile.getName());
+        }
+    }
+
+    private boolean equalLists(List<String> l1, Set<String> l2) {
+        // Check if l1 and l2 have the same items, regardless of order
+        if (l1.size() != l2.size()) {
+            return false;
+        }
+        for (String item : l1) {
+            if (!l2.contains(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<String> getInputStreamKeys(InputStream defConfigStream) {
+        List<String> keys = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(defConfigStream));
+        try {
+            // WhiteSpace, Keys
+            Map<String, List<String>> map = new HashMap<>();
+
+            Pattern pattern = Pattern.compile("^( *)([A-z0-9-_]+):");
+
+            List<String> building = new ArrayList<>();
+            int lastWhitespace = -1;
+            while(reader.ready()) {
+                String line = reader.readLine();
+                Matcher m = pattern.matcher(line);
+                if (m.find()) {
+                    String whiteSpace = m.group(1);
+                    String key = m.group(2);
+
+                    if (whiteSpace.length() > lastWhitespace) {
+                        building.add(key);
+                        lastWhitespace = whiteSpace.length();
+                    }else if (whiteSpace.length() == lastWhitespace) {
+                        //Form a key from the previous additions
+                        StringBuilder sb = new StringBuilder();
+                        for (String s : building) {
+                            sb.append(s).append(".");
+                        }
+                        keys.add(sb.substring(0, sb.length() - 1));
+
+                        //Set the last one to this key so we may continue
+                        building.set(building.size() - 1, key);
+                    }else {
+                        StringBuilder sb = new StringBuilder();
+                        if (whiteSpace.length() == 0) {
+                            //Form a key from the previous additions
+                            for (String s : building) {
+                                sb.append(s).append(".");
+                            }
+                            keys.add(sb.substring(0, sb.length() - 1));
+
+                            building.clear();
+                            building.add(key);
+                            lastWhitespace = 0;
+                        }else {
+                            //Form a key from the previous additions
+                            for (String s : building) {
+                                sb.append(s).append(".");
+                            }
+                            keys.add(sb.substring(0, sb.length() - 1));
+
+                            int diff = lastWhitespace - whiteSpace.length();
+                            int times = diff / 2;
+
+                            // Remove the amount of keys equals to number of tabs +1 (to make space for new key)
+                            for (int i = 0; i <= times; i++) {
+                                if (building.size() > 0) {
+                                    building.remove(building.size() - 1);
+                                }
+                            }
+
+                            //Set the last one to this key so we may continue
+                            building.add(key);
+                            lastWhitespace = whiteSpace.length();
+                        }
+                    }
+                }
+            }
+
+            //Form a key from whatever is left
+            StringBuilder sb = new StringBuilder();
+            for (String s : building) {
+                sb.append(s).append(".");
+            }
+            keys.add(sb.substring(0, sb.length() - 1));
+        }catch (Exception e) { e.printStackTrace(); }
+        return keys;
     }
 
     public static class ANSI {
