431219a5bac2f4de8e022f34e53a0875577fe839
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-12 12:30:19 -0800

    v1.3.5.7 - Capitalize method

--- src/main/java/com/kamikazejamplugins/kamicommon/util/StringUtil.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/util/StringUtil.java
@@ -127,4 +127,12 @@ public class StringUtil {
         }
         return reversed.toArray(new String[0]);
     }
+
+    public static String capitalize(String str) {
+        if(str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 }
