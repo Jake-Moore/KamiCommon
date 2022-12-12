8cf0ad7d81cdf0b63a154b43df69ad6095eb9b63
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2022-12-12 12:46:01 -0800

    v1.3.5.8 - StringUtil.IntegerToRomanNumeral

--- src/main/java/com/kamikazejamplugins/kamicommon/util/StringUtil.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/util/StringUtil.java
@@ -135,4 +135,62 @@ public class StringUtil {
 
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
+
+    public static String IntegerToRomanNumeral(int input) {
+        if (input < 1 || input > 3999)
+            return "Invalid Roman Number Value";
+        StringBuilder s = new StringBuilder();
+        while (input >= 1000) {
+            s.append("M");
+            input -= 1000;        }
+        while (input >= 900) {
+            s.append("CM");
+            input -= 900;
+        }
+        while (input >= 500) {
+            s.append("D");
+            input -= 500;
+        }
+        while (input >= 400) {
+            s.append("CD");
+            input -= 400;
+        }
+        while (input >= 100) {
+            s.append("C");
+            input -= 100;
+        }
+        while (input >= 90) {
+            s.append("XC");
+            input -= 90;
+        }
+        while (input >= 50) {
+            s.append("L");
+            input -= 50;
+        }
+        while (input >= 40) {
+            s.append("XL");
+            input -= 40;
+        }
+        while (input >= 10) {
+            s.append("X");
+            input -= 10;
+        }
+        while (input >= 9) {
+            s.append("IX");
+            input -= 9;
+        }
+        while (input >= 5) {
+            s.append("V");
+            input -= 5;
+        }
+        while (input >= 4) {
+            s.append("IV");
+            input -= 4;
+        }
+        while (input >= 1) {
+            s.append("I");
+            input -= 1;
+        }
+        return s.toString();
+    }
 }
