e9e9f893a4fdd98dba8c38b45dd0f9554fdb0e80
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2023-01-12 00:36:31 -0800

    v1.4.1 - Fix running as a plugin.

--- src/main/java/com/kamikazejamplugins/kamicommon/KamiCommon.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/KamiCommon.java
@@ -1,5 +1,6 @@
 package com.kamikazejamplugins.kamicommon;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /*---
@@ -11,13 +12,14 @@ public class KamiCommon extends JavaPlugin {
     private static KamiCommon plugin;
 
     @Override
-    public void onEnable() {
+    public void onEnable(){
         plugin = this;
+        Bukkit.getLogger().info("KamiCommon enabled");
     }
 
     @Override
     public void onDisable() {
-
+        Bukkit.getLogger().info("KamiCommon disabled");
     }
 
     public static JavaPlugin get() {
