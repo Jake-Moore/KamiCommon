4a0718608abd829270b02edb28d22b66ff79cde2
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2023-01-30 21:21:54 -0800

    v1.5.12 - Work on the BlockUtil

--- src/main/java/com/kamikazejamplugins/kamicommon/nms/block/BlockUtil1_17_R1.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/nms/block/BlockUtil1_17_R1.java
@@ -1,7 +1,7 @@
 package com.kamikazejamplugins.kamicommon.nms.block;
 
-import com.cryptomorin.xseries.XMaterial;
 import com.kamikazejamplugins.kamicommon.KamiCommon;
+import com.kamikazejamplugins.kamicommon.util.MaterialData;
 import net.minecraft.core.BlockPosition;
 import net.minecraft.server.level.WorldServer;
 import net.minecraft.world.level.block.state.IBlockData;
@@ -9,21 +9,31 @@ import net.minecraft.world.level.chunk.Chunk;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
 
-public class BlockUtil1_17_R1 implements IBlockUtil {
+@SuppressWarnings("deprecation")
+public class BlockUtil1_17_R1 extends IBlockUtil {
+
     @Override
-    public void setBlockSuperFast(Block b, XMaterial xMaterial, boolean lightUpdate, boolean physics) {
+    public boolean supportsCombined() { return false; }
+
+    @Override
+    public void setMaterialData(Block b, MaterialData materialData, boolean lightUpdate, boolean physics) {
+
+        if (materialData.getData() != 0) {
+            set1_13BlockData(b, materialData, lightUpdate, physics);
+            return;
+        }
 
         WorldServer w = ((CraftWorld) b.getWorld()).getHandle();
         Chunk chunk = w.k().a(b.getX() >> 4, b.getZ() >> 4, true); // load chunk (if not loaded)
 
         if (chunk == null) {
             KamiCommon.get().getLogger().warning("[BlockUtil] Chunk is null! " + b.getX() + ", " + b.getZ());
-            b.setType(xMaterial.parseMaterial());
+            b.setType(materialData.getMaterial());
             return;
         }
 
         BlockPosition bp = new BlockPosition(b.getX(), b.getY(), b.getZ());
-        IBlockData ibd = net.minecraft.world.level.block.Block.a(xMaterial.getId());
+        IBlockData ibd = net.minecraft.world.level.block.Block.a(materialData.getMaterial().getId());
 
         // 1.19 is weird, this is the best I can think to do
         chunk.setBlockState(bp, ibd, false, true);
