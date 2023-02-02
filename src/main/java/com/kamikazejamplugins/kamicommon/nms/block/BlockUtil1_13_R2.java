f8053737becb3ebc066ff11c298e51668413b135
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2023-02-01 20:14:25 -0800

    v1.5.19 - Add back a few I think will work

--- src/main/java/com/kamikazejamplugins/kamicommon/nms/block/BlockUtil1_13_R2.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/nms/block/BlockUtil1_13_R2.java
@@ -1,8 +1,29 @@
 package com.kamikazejamplugins.kamicommon.nms.block;
 
+import net.minecraft.server.v1_13_R2.BlockPosition;
+import net.minecraft.server.v1_13_R2.Chunk;
+import net.minecraft.server.v1_13_R2.IBlockData;
+import net.minecraft.server.v1_13_R2.WorldServer;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
+
 public class BlockUtil1_13_R2 extends IBlockUtil {
-
     @Override
-    boolean supportsCombined() { return false; }
+    public void setCombined(Block b, int combined, boolean lightUpdate, boolean physics) {
+        WorldServer w = ((CraftWorld) b.getWorld()).getHandle();
+        Chunk chunk = w.getChunkAt(b.getX() >> 4, b.getZ() >> 4);
+        BlockPosition bp = new BlockPosition(b.getX(), b.getY(), b.getZ());
+        IBlockData ibd = net.minecraft.server.v1_13_R2.Block.getByCombinedId(combined);
 
+        if (lightUpdate) {
+            w.setTypeAndData(bp, ibd, (physics) ? 3 : 2); // applyPhysics = 3, 2 is none
+        }else {
+            try {
+                chunk.a(bp, combined, physics);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+            w.notify(bp, ibd, ibd, 0);
+        }
+    }
 }
