7865863cd2db98fa0f2df8b9f0bc72a3164e8f1e
Jake Moore <38172916+Jake-Moore@users.noreply.github.com>
2023-04-01 19:16:42 -0700

    v1.6.1 - Update KamiSubCommands to require a plugin instance in order to check the proper jar for versioning.

--- src/main/java/com/kamikazejamplugins/kamicommon/command/impl/KamiVerCmd.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/command/impl/KamiVerCmd.java
@@ -3,12 +3,18 @@ package com.kamikazejamplugins.kamicommon.command.impl;
 import com.kamikazejamplugins.kamicommon.VersionControl;
 import com.kamikazejamplugins.kamicommon.command.KamiSubCommand;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class KamiVerCmd extends KamiSubCommand {
+    private final JavaPlugin plugin;
+
+    public KamiVerCmd(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public List<String> getNames() { return Arrays.asList("ver", "version"); }
@@ -24,7 +30,7 @@ public class KamiVerCmd extends KamiSubCommand {
 
     @Override
     public boolean performCommand(CommandSender sender, String label, String[] args) {
-        VersionControl.sendDetails(sender);
+        VersionControl.sendDetails(plugin, sender);
         return true;
     }
 }
