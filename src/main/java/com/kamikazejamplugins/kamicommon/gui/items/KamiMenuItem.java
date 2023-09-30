f79b37bbbbfb73aad4cab4b36dd40926346f0839
Jake Moore <jakealexandermoore@icloud.com>
2023-09-30 01:58:44 -0700

    v1.8.2.0 - KamiMenu rework to support better pagination in KamiMenuContainer

--- src/main/java/com/kamikazejamplugins/kamicommon/gui/items/KamiMenuItem.java
+++ src/main/java/com/kamikazejamplugins/kamicommon/gui/items/KamiMenuItem.java
@@ -1,11 +1,13 @@
 package com.kamikazejamplugins.kamicommon.gui.items;
 
-import com.kamikazejamplugins.kamicommon.gui.interfaces.MenuClickInfo;
+import com.kamikazejamplugins.kamicommon.gui.interfaces.MenuClick;
+import com.kamikazejamplugins.kamicommon.gui.page.PageItem;
 import com.kamikazejamplugins.kamicommon.item.IAItemBuilder;
 import com.kamikazejamplugins.kamicommon.item.IBuilder;
 import com.kamikazejamplugins.kamicommon.yaml.ConfigurationSection;
 import lombok.Getter;
 import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -13,11 +15,10 @@ import java.util.List;
 
 @Getter
 @SuppressWarnings({"unused", "UnusedReturnValue"})
-public class KamiMenuItem {
+public class KamiMenuItem extends PageItem {
+
     private boolean enabled;
-    private IBuilder iBuilder;
     private List<Integer> slots;
-    @Nullable private MenuClickInfo clickInfo = null;
 
     // For like KamiMenuItem(config, "menus.menu1")
     public KamiMenuItem(ConfigurationSection section, String key) {
@@ -26,7 +27,6 @@ public class KamiMenuItem {
     public KamiMenuItem(ConfigurationSection section, String key, OfflinePlayer player) {
         this(section.getConfigurationSection(key));
     }
-
 
 
     // For calls like KamiMenuItem(config.getConfigurationSection("menus.menu1"))
@@ -38,8 +38,8 @@ public class KamiMenuItem {
      * @param player Only required for PLAYER_HEAD ItemStacks, player is used as the skullOwner
      */
     public KamiMenuItem(ConfigurationSection section, @Nullable OfflinePlayer player) {
+        super(new IAItemBuilder(section, player), (MenuClick) null); // Null safe for player arg
         enabled = section.getBoolean("enabled", true);
-        iBuilder = new IAItemBuilder(section, player); // Null safe for player arg
 
         slots = new ArrayList<>();
         if (section.isInt("slot")) {
@@ -54,14 +54,14 @@ public class KamiMenuItem {
     }
 
     public KamiMenuItem(boolean enabled, IBuilder builder, List<Integer> slots) {
+        super(builder, (MenuClick) null);
         this.enabled = enabled;
-        this.iBuilder = builder;
         this.slots = slots;
     }
 
     public KamiMenuItem(boolean enabled, IBuilder builder, int slot) {
+        super(builder, (MenuClick) null);
         this.enabled = enabled;
-        this.iBuilder = builder;
         this.slots = new ArrayList<>();
         this.slots.add(slot);
     }
@@ -71,7 +71,8 @@ public class KamiMenuItem {
         return this;
     }
 
-    public KamiMenuItem setIBuilder(IBuilder iBuilder) {
+    @Override
+    public KamiMenuItem setIBuilder(@NotNull IBuilder iBuilder) {
         this.iBuilder = iBuilder;
         return this;
     }
@@ -88,8 +89,9 @@ public class KamiMenuItem {
     }
 
 
-    public KamiMenuItem setClickInfo(MenuClickInfo clickInfo) {
-        this.clickInfo = clickInfo;
+    @Override
+    public KamiMenuItem setMenuClick(MenuClick menuClick) {
+        this.menuClick = menuClick;
         return this;
     }
 }
