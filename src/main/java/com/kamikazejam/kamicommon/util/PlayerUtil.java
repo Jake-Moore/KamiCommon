cecae73d71f1315be5b50a79a96d3555d44b5106
Jake Moore <jakealexandermoore@icloud.com>
2024-01-04 16:13:14 -0800

    2.2.1.6 -Adds PlayerUtil.takeItems
    Took 11 minutes

--- src/main/java/com/kamikazejam/kamicommon/util/PlayerUtil.java
+++ src/main/java/com/kamikazejam/kamicommon/util/PlayerUtil.java
@@ -1,10 +1,13 @@
 package com.kamikazejam.kamicommon.util;
 
 import com.cryptomorin.xseries.XMaterial;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utility class for helping with players
@@ -85,6 +88,74 @@ public class PlayerUtil {
             }
         }
         return itemStack;
+    }
+
+    /**
+     * Will try to take the items from the player's inventory
+     * @param enforceQuantities If we should fail if we cannot take all the items
+     * @return True IFF all items were taken
+     */
+    public static boolean takeItems(Player player, boolean enforceQuantities, ItemStack... itemStacks) {
+        if (enforceQuantities && !canTakeItems(player, itemStacks)) {
+            return false;
+        }
+        for (ItemStack itemStack : itemStacks) {
+            takeItem(player, itemStack);
+        }
+        return true;
+    }
+
+    private static void takeItem(Player player, ItemStack itemStack) {
+        int amount = itemStack.getAmount();
+
+        // Loop through the inventory removing/subtracting amounts until we hit quota
+        for (ItemStack stack : player.getInventory().getContents()) {
+            if (stack == null || stack.getType() == Material.AIR || stack.getAmount() < 1) { continue; }
+            if (!stack.isSimilar(itemStack)) { continue; }
+
+            int stackAmount = stack.getAmount();
+            if (stackAmount > amount) {
+                stack.setAmount(stackAmount - amount);
+                return;
+            } else if (stackAmount == amount) {
+                player.getInventory().remove(stack);
+                return;
+            } else {
+                amount -= stackAmount;
+                player.getInventory().remove(stack);
+            }
+        }
+    }
+
+    public static boolean canTakeItems(Player player, ItemStack... itemStacks) {
+        // Create a map of Items to take, and their quantities
+        Map<ItemStack, Integer> takeAmounts = new HashMap<>();
+        for (ItemStack itemStack : itemStacks) {
+            ItemStack clone = itemStack.clone(); clone.setAmount(1);
+
+            int amount = takeAmounts.getOrDefault(clone, 0);
+            takeAmounts.put(clone, amount + itemStack.getAmount());
+        }
+
+        // Create a map of Player's items, and their quantities
+        Map<ItemStack, Integer> playerAmounts = new HashMap<>();
+        for (ItemStack stack : player.getInventory().getContents()) {
+            if (stack == null || stack.getType() == Material.AIR || stack.getAmount() < 1) { continue; }
+            ItemStack clone = stack.clone(); clone.setAmount(1);
+
+            int amount = playerAmounts.getOrDefault(clone, 0);
+            playerAmounts.put(clone, amount + stack.getAmount());
+        }
+
+        // Check if the player has enough of each item
+        for (Map.Entry<ItemStack, Integer> entry : takeAmounts.entrySet()) {
+            ItemStack itemStack = entry.getKey();
+            int amount = entry.getValue();
+
+            int playerAmount = playerAmounts.getOrDefault(itemStack, 0);
+            if (playerAmount < amount) { return false; }
+        }
+        return true;
     }
 
 }
