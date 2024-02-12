dea2fa7c5129dae4169794d16fc82d2ff7892bfc
Jake Moore <jakealexandermoore@icloud.com>
2024-02-11 23:54:08 -0800

    2.2.3.5 -Fixed NPE after module prefix system was changed.
    Took 6 minutes

---
+++ src/main/java/com/kamikazejam/kamicommon/util/Preconditions.java
@@ -0,0 +1,11 @@
+package com.kamikazejam.kamicommon.util;
+
+public class Preconditions {
+    public static <T> T checkNotNull(T reference) {
+        if (reference == null) {
+            throw new NullPointerException();
+        } else {
+            return reference;
+        }
+    }
+}
