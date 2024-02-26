0dc12091a2ee7a332c7344731a9f215c1c8e81e9
Jake Moore <jakealexandermoore@icloud.com>
2024-02-25 23:55:03 -0800

    2.2.3.6 -Adds CmdModules.java
    Took 6 minutes

---
+++ src/main/java/com/kamikazejam/kamicommon/modules/commands/CmdModules.java
@@ -0,0 +1,48 @@
+package com.kamikazejam.kamicommon.modules.commands;
+
+import com.kamikazejam.kamicommon.KamiPlugin;
+import com.kamikazejam.kamicommon.command.KamiCommand;
+import com.kamikazejam.kamicommon.command.requirement.RequirementHasPerm;
+import com.kamikazejam.kamicommon.modules.Module;
+import com.kamikazejam.kamicommon.util.StringUtil;
+import com.kamikazejam.kamicommon.util.exception.KamiCommonException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class CmdModules extends KamiCommand {
+    private final KamiPlugin plugin;
+    public CmdModules(KamiPlugin plugin, String permission) {
+        this.plugin = plugin;
+        addAliases("modules");
+
+        addRequirements(RequirementHasPerm.get(permission));
+
+        setDesc("List all modules");
+    }
+
+    @Override
+    public void perform() throws KamiCommonException {
+        sender.sendMessage(" ");
+        sender.sendMessage(StringUtil.t("&eModules:"));
+        // Loop through moduleList in alphabetical order
+        List<Module> moduleList = plugin.getModuleManager().getModuleList();
+
+        List<Module> modules = new ArrayList<>(moduleList);
+        modules.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+
+        for (Module module : modules) {
+            int p = moduleList.indexOf(module) + 1;
+            String pos = (p < 10 ? " " + p : String.valueOf(p));
+
+            if (module.isEnabled() && module.isSuccessfullyEnabled()) {
+                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &a" + String.valueOf(module.isEnabled()).toUpperCase()));
+            }else if (!module.isSuccessfullyEnabled()) {
+                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &c" + String.valueOf(module.isEnabled()).toUpperCase()));
+            }else {
+                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &6Disabled"));
+            }
+        }
+    }
+}
