5f68b391c99cca4f7c1e1d1e12594c0ba96071b8
Jake Moore <jakealexandermoore@icloud.com>
2024-04-07 04:38:29 -0700

    -fix CI? maybe
    Took 5 minutes

--- refresh.sh
+++ refresh.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "./gradlew :standalone-utils:build"
-./gradlew :standalone-utils:build || exit 1
+root_command="$root_command"
 
-echo "./gradlew :generic-jar:build"
-./gradlew :generic-jar:build || exit 1
+# Check if GITHUB_REPOSITORY is present
+if [ -n "$GITHUB_REPOSITORY" ]; then
+  root_command="gradle"
+fi
 
-echo "./gradlew :spigot-nms:api:build"
-./gradlew :spigot-nms:api:build || exit 1
+echo "$root_command :standalone-utils:build"
+"$root_command" :standalone-utils:build || exit 1
 
-echo "./gradlew :spigot-nms:build"
-./gradlew :spigot-nms:build || exit 1
+echo "$root_command :generic-jar:build"
+"$root_command" :generic-jar:build || exit 1
 
-echo "./gradlew :spigot-utils:build"
-./gradlew :spigot-utils:build || exit 1
+echo "$root_command :spigot-nms:api:build"
+"$root_command" :spigot-nms:api:build || exit 1
+
+echo "$root_command :spigot-nms:build"
+"$root_command" :spigot-nms:build || exit 1
+
+echo "$root_command :spigot-utils:build"
+"$root_command" :spigot-utils:build || exit 1
