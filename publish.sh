a1ee11e4057f6b4a8f24cb61d39d9db4f2f30182
Jake Moore <jakealexandermoore@icloud.com>
2024-04-07 04:33:33 -0700

    -fix CI?
    Took 5 minutes

--- publish.sh
+++ publish.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-echo "./gradlew clean"
-./gradlew clean || exit 1
+# Check if a command is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <command>"
+    exit 1
+fi
+root_command="$1"
 
-echo "./gradlew :standalone-utils:publish"
-./gradlew :standalone-utils:publish || exit 1
+echo "$root_command clean"
+"$root_command" clean || exit 1
 
-echo "./gradlew :generic-jar:publish"
-./gradlew :generic-jar:publish || exit 1
+echo "$root_command :standalone-utils:publish"
+"$root_command" :standalone-utils:publish || exit 1
 
-echo "./gradlew :standalone-jar:publish"
-./gradlew :standalone-jar:publish || exit 1
+echo "$root_command :generic-jar:publish"
+"$root_command" :generic-jar:publish || exit 1
+
+echo "$root_command :standalone-jar:publish"
+"$root_command" :standalone-jar:publish || exit 1
 
 # Build these 3 for spigot-utils to have the latest changes
-echo "./gradlew :spigot-nms:api:build"
-./gradlew :spigot-nms:api:build || exit 1
+echo "$root_command :spigot-nms:api:build"
+"$root_command" :spigot-nms:api:build || exit 1
 
-echo "./gradlew :spigot-nms:build"
-./gradlew :spigot-nms:build || exit 1
+echo "$root_command :spigot-nms:build"
+"$root_command" :spigot-nms:build || exit 1
 
-echo "./gradlew :spigot-utils:publish"
-./gradlew :spigot-utils:publish || exit 1
+echo "$root_command :spigot-utils:publish"
+"$root_command" :spigot-utils:publish || exit 1
 
-echo "./gradlew :spigot-jar:publish"
-./gradlew :spigot-jar:publish || exit 1
+echo "$root_command :spigot-jar:publish"
+"$root_command" :spigot-jar:publish || exit 1
