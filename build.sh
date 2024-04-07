fdbc8062683fea41d79230422fdbe48917c117a7
Jake Moore <jakealexandermoore@icloud.com>
2024-04-07 04:33:33 -0700

    -fix CI?
    Took 5 minutes

--- build.sh
+++ build.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-echo "./gradlew clean"
-./gradlew clean || exit 1
+# Check if a command is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <command>"
+    exit 1
+fi
+root_command="$1"
 
-echo "./gradlew :standalone-utils:build"
-./gradlew :standalone-utils:build || exit 1
+echo "$root_command clean"
+"$root_command" clean || exit 1
 
-echo "./gradlew :generic-jar:build"
-./gradlew :generic-jar:build || exit 1
+echo "$root_command :standalone-utils:build"
+"$root_command" :standalone-utils:build || exit 1
 
-echo "./gradlew :standalone-jar:build"
-./gradlew :standalone-jar:build || exit 1
+echo "$root_command :generic-jar:build"
+"$root_command" :generic-jar:build || exit 1
 
-echo "./gradlew :spigot-nms:api:build"
-./gradlew :spigot-nms:api:build || exit 1
+echo "$root_command :standalone-jar:build"
+"$root_command" :standalone-jar:build || exit 1
 
-echo "./gradlew :spigot-nms:build"
-./gradlew :spigot-nms:build || exit 1
+echo "$root_command :spigot-nms:api:build"
+"$root_command" :spigot-nms:api:build || exit 1
 
-echo "./gradlew :spigot-utils:build"
-./gradlew :spigot-utils:build || exit 1
+echo "$root_command :spigot-nms:build"
+"$root_command" :spigot-nms:build || exit 1
 
-echo "./gradlew :spigot-jar:build"
-./gradlew :spigot-jar:build || exit 1
+echo "$root_command :spigot-utils:build"
+"$root_command" :spigot-utils:build || exit 1
+
+echo "$root_command :spigot-jar:build"
+"$root_command" :spigot-jar:build || exit 1
