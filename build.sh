0d47e66fe0ada9a99a6b77aa44a03a658542ddfc
Jake Moore <jakealexandermoore@icloud.com>
2024-04-07 01:03:43 -0700

    -fix build and publish (order of operations and depends)
    Took 5 minutes

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ echo "./gradlew clean"
 echo "./gradlew :standalone-utils:build"
 ./gradlew :standalone-utils:build || exit 1
 
-echo "./gradlew :standalone-jar:build"
-./gradlew :standalone-jar:build || exit 1
-
 echo "./gradlew :generic-jar:build"
 ./gradlew :generic-jar:build || exit 1
+
+echo "./gradlew :standalone-jar:build"
+./gradlew :standalone-jar:build || exit 1
 
 echo "./gradlew :spigot-nms:api:build"
 ./gradlew :spigot-nms:api:build || exit 1
