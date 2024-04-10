019116546dc440aaa7f26cd9a50637e976eda73b
Jake Moore <jakealexandermoore@icloud.com>
2024-04-09 18:35:23 -0700

    3.0.0.0 -Release 3.0.0.0 -Include other jars in the release
    Took 5 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.gradle.ext.settings
 import org.jetbrains.gradle.ext.taskTriggers
 
-var VERSION = "3.0.0.0-pre2-SNAPSHOT"
+var VERSION = "3.0.0.0"
 
 plugins { // needed for the subprojects section to work
     id("java")
