a9a8edfa427a2411fb1261e519abcb6d975a3ceb
Jake Moore <jakealexandermoore@icloud.com>
2024-05-14 02:47:15 -0700

    v3.0.1.4 - Fixes

--- spigot-utils/src/main/java/com/kamikazejam/kamicommon/configuration/config/KamiConfigExt.java
+++ spigot-utils/src/main/java/com/kamikazejam/kamicommon/configuration/config/KamiConfigExt.java
@@ -20,6 +20,10 @@ public class KamiConfigExt extends KamiConfig {
     public KamiConfigExt(@NotNull JavaPlugin plugin, File file, boolean addDefaults) {
         super(plugin, file, addDefaults);
     }
+    public KamiConfigExt(@NotNull JavaPlugin plugin, File file, boolean addDefaults, boolean strictKeys) {
+        super(plugin, file, addDefaults);
+        this.setStrictKeys(strictKeys);
+    }
     public KamiConfigExt(@NotNull JavaPlugin plugin, File file, Supplier<InputStream> defaultSupplier) {
         super(plugin, file, defaultSupplier);
     }
