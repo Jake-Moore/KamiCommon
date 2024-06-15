0507dc9dfba4d2504107fc28f92cbb0f1dce3fe8
Jake Moore <jakealexandermoore@icloud.com>
2024-06-15 12:53:31 -0700

    v3.0.2.7 - Reformatted NmsTest for scalability

--- spigot-jar/src/main/java/com/kamikazejam/kamicommon/command/internal/CmdNmsTest.java
+++ spigot-jar/src/main/java/com/kamikazejam/kamicommon/command/internal/CmdNmsTest.java
@@ -21,6 +21,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.awt.Color;
+import java.util.List;
 
 @SuppressWarnings({"SpellCheckingInspection", "unused"})
 public class CmdNmsTest extends KamiCommand {
@@ -31,22 +32,18 @@ public class CmdNmsTest extends KamiCommand {
         addRequirements(RequirementIsPlayer.get());
     }
 
-    @Override
-    public void perform() {
-        Player plr = (Player) sender;
-        plr.sendMessage(StringUtil.t("&7NMS Version: &f" + NmsVersion.getMCVersion() + " &7(&f" + NmsVersion.getFormattedNmsInteger() + "&7)"));
-        plr.sendMessage(StringUtil.t("  &7WineSpigot?: &f" + NmsVersion.isWineSpigot()));
 
+    private static final List<Test> tests = List.of(
         // Chat Color Provider Test
-        boolean t1 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing ChatColorProvider..."));
             ChatColorProvider ccProvider = NmsAPI.getChatColorProvider();
             Color jColor = ccProvider.get().getColor(ChatColor.AQUA);
             player.sendMessage(StringUtil.t("    &7Success"));
-        }, plr);
+        },
 
         // Block Util Provider Test
-        boolean t2 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing BlockUtilProvider..."));
             BlockUtilProvider buProvider = NmsAPI.getBlockUtilProvider();
             Block block = Bukkit.getWorlds().getFirst().getBlockAt(0, 0, 0);
@@ -56,10 +53,10 @@ public class CmdNmsTest extends KamiCommand {
             buProvider.get().setBlockSuperFast(block, XMaterial.EMERALD_BLOCK, PlaceType.NMS);
             block.setType(oldType);
             player.sendMessage(StringUtil.t("    &7Success"));
-        }, plr);
+        },
 
         // MessageManager Test
-        boolean t3 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing MessageManager..."));
             ItemStack item = new ItemStack(Material.DIAMOND_SWORD);
             ItemMeta meta = item.getItemMeta();
@@ -73,32 +70,47 @@ public class CmdNmsTest extends KamiCommand {
             Action combined = new Action("<6>", "&fCombined").setClickSuggestCommand("help").setHoverText(StringUtil.t("&bThis is hover text"));
             String message = "Test: <1> <2> <3> <4> <5> <6>";
             NmsAPI.getMessageManager().processAndSend(player, message, clickCmd, clickSug, clickUrl, hoverText, hoverItem, combined);
-        }, plr);
+        },
 
         // Teleport Provider Test
-        boolean t4 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing TeleportProvider..."));
             NmsAPI.getTeleporter().teleportWithoutEvent(player, player.getLocation().clone().add(0, 0.5, 0));
             player.sendMessage(StringUtil.t("    &7Success"));
-        }, plr);
+        },
 
         // Main Hand Provider
-        boolean t5 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing MainHandProvider..."));
             ItemStack stack = NmsAPI.getItemInMainHand(player);
             player.sendMessage(StringUtil.t("    &7Success: " + (stack == null ? "AIR" : stack.getType().name())));
-        }, plr);
+        },
 
         // Enchant ID Provider
-        boolean t6 = runTest((player) -> {
+        (player) -> {
             player.sendMessage(StringUtil.t("&7Testing EnchantIDProvider..."));
             player.sendMessage(StringUtil.t("    &7Success: " + NmsAPI.getNamespaced(Enchantment.DAMAGE_ALL)));
-        }, plr);
+        }
+    );
 
-        if (t1 && t2 && t3 && t4 && t5 && t6) {
-            plr.sendMessage(StringUtil.t("&aALL TESTS PASSED!"));
+
+
+    @Override
+    public void perform() {
+        Player player = (Player) sender;
+        player.sendMessage(StringUtil.t("&7NMS Version: &f" + NmsVersion.getMCVersion() + " &7(&f" + NmsVersion.getFormattedNmsInteger() + "&7)"));
+        player.sendMessage(StringUtil.t("  &7WineSpigot?: &f" + NmsVersion.isWineSpigot()));
+
+        int successes = 0;
+        for (Test test : tests) {
+            if (runTest(test, player)) { successes++; }
+        }
+
+        // Send Results
+        if (successes >= tests.size()) {
+            player.sendMessage(StringUtil.t("&aALL TESTS PASSED! (" + successes + "/" + tests.size() + ")"));
         }else {
-            plr.sendMessage(StringUtil.t("&cTEST SUITE FAILED! See Console."));
+            player.sendMessage(StringUtil.t("&cTEST SUITE FAILED! (" + successes + "/" + tests.size() + ") &lSee Console."));
         }
     }
 
