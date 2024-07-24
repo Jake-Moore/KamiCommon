501bf41cf7e9989328fc322156d314c79d0d5e8e
Jake Moore <jakealexandermoore@icloud.com>
2024-07-23 18:53:40 -0700

    v3.3.0.7 -fix

--- publishLocal.sh
+++ publishLocal.sh
@@ -7,16 +7,19 @@ if [ -z "$GITHUB_REPOSITORY" ]; then
   "$root_command" clean || exit 1
 fi
 
-echo "$root_command :standalone-utils:publishToMavenLocal"
+echo "$root_command :standalone-utils:build"
 "$root_command" :standalone-utils:build || exit 1
+echo "$root_command :standalone-utils:publishToMavenLocal"
 "$root_command" :standalone-utils:publishToMavenLocal || exit 1
 
-echo "$root_command :generic-jar:publishToMavenLocal"
+echo "$root_command :generic-jar:build"
 "$root_command" :generic-jar:build || exit 1
+echo "$root_command :generic-jar:publishToMavenLocal"
 "$root_command" :generic-jar:publishToMavenLocal || exit 1
 
-echo "$root_command :standalone-jar:publishToMavenLocal"
+echo "$root_command :standalone-jar:build"
 "$root_command" :standalone-jar:build || exit 1
+echo "$root_command :standalone-jar:publishToMavenLocal"
 "$root_command" :standalone-jar:publishToMavenLocal || exit 1
 
 echo "$root_command :spigot-nms:api:build"
@@ -25,10 +28,12 @@ echo "$root_command :spigot-nms:api:build"
 echo "$root_command :spigot-nms:build"
 "$root_command" :spigot-nms:build || exit 1
 
-echo "$root_command :spigot-utils:publishToMavenLocal"
+echo "$root_command :spigot-utils:build"
 "$root_command" :spigot-utils:build || exit 1
+echo "$root_command :spigot-utils:publishToMavenLocal"
 "$root_command" :spigot-utils:publishToMavenLocal || exit 1
 
-echo "$root_command :spigot-jar:publishToMavenLocal"
+echo "$root_command :spigot-jar:build"
 "$root_command" :spigot-jar:build || exit 1
+echo "$root_command :spigot-jar:publishToMavenLocal"
 "$root_command" :spigot-jar:publishToMavenLocal || exit 1
