d85a49e7efd1081157ee50a71e7be6837bbf258e
Jake Moore <jakealexandermoore@icloud.com>
2024-08-14 14:08:19 -0700

    v3.3.1.8 -Update PreSpawnSpawnerEvent clarity and event implementation
    Took 19 minutes

--- spigot-nms/api/src/main/java/com/kamikazejam/kamicommon/nms/abstraction/event/PreSpawnSpawnerEvent.java
+++ spigot-nms/api/src/main/java/com/kamikazejam/kamicommon/nms/abstraction/event/PreSpawnSpawnerEvent.java
@@ -6,27 +6,29 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.block.BlockEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Pre 1.12:
- * - 1.8.8 assumes that TacoSpigot is being used (best compatability)
+ * - 1.8.8 assumes that TacoSpigot is being used (best compatability, but doesn't include spawn location)
  * - Other versions just require Spigot (worse compatability however)
  * 1.13+:
  * - Requires PaperSpigot, wraps native event (best compatability)
  */
 @Getter
 @SuppressWarnings("unused")
-public class PreSpawnSpawnerEvent extends BlockEvent implements Cancellable {
+public class PreSpawnSpawnerEvent extends Event implements Cancellable {
 
-    private final @NotNull Location location;
+    private final @NotNull Block spawnerBlock;
+    private final @Nullable Location spawnLocation;
     private final @NotNull EntityType type;
 
-    public PreSpawnSpawnerEvent(@NotNull Block theBlock, @NotNull EntityType type) {
-        super(theBlock);
-        this.location = theBlock.getLocation();
+    public PreSpawnSpawnerEvent(@NotNull Block spawnerBlock, @NotNull EntityType type, @Nullable Location spawnLocation) {
+        this.spawnerBlock = spawnerBlock;
+        this.spawnLocation = spawnLocation;
         this.type = type;
     }
 
