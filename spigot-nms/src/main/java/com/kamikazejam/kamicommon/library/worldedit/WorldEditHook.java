5af0cbc6bb56e4c1b6feec3e2bc33fd4d8274122
Jake Moore <jakealexandermoore@icloud.com>
2024-08-25 00:25:41 -0700

    v3.4.0.7 -FAWE support in WorldEditHook
    Took 8 minutes

--- spigot-nms/src/main/java/com/kamikazejam/kamicommon/library/worldedit/WorldEditHook.java
+++ spigot-nms/src/main/java/com/kamikazejam/kamicommon/library/worldedit/WorldEditHook.java
@@ -1,6 +1,5 @@
 package com.kamikazejam.kamicommon.library.worldedit;
 
-import com.kamikazejam.kamicommon.library.worldedit.WorldEditApi;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -24,8 +23,22 @@ public class WorldEditHook {
             return worldEditVer = new WorldEdit6();
         } else if (ver.startsWith("7")) {
             return worldEditVer = new WorldEdit7();
-        }else {
-            return null;
         }
+
+        // Try to fetch FAWE (for later versions WorldEdit is not required to run FAWE)
+        // So if we find FAWE without WE, we can assume WE 7
+        Plugin fawe = Bukkit.getServer().getPluginManager().getPlugin("FastAsyncWorldEdit");
+        if (fawe != null && fawe.isEnabled()) {
+            try {
+                // If this class is found, it's v7 WorldEdit within FAWE
+                Class<?> v7Specific = Class.forName("com.sk89q.worldedit.math.BlockVector3");
+                return worldEditVer = new WorldEdit7();
+            }catch (Throwable ignored) {
+                // If we have an error, try falling back to WE 6
+                return worldEditVer = new WorldEdit6();
+            }
+        }
+
+        return null;
     }
 }
