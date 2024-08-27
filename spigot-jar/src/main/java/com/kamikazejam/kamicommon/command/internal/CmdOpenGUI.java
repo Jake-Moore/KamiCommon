0f85e386b3a9dc12302f7e655c280635b76730c7
Jake Moore <jakealexandermoore@icloud.com>
2024-08-26 19:08:58 -0700

    v3.4.0.13 -Added ``/kc opengui paged` sample gui, for verification of paged guis.
    Took 8 minutes

--- spigot-jar/src/main/java/com/kamikazejam/kamicommon/command/internal/CmdOpenGUI.java
+++ spigot-jar/src/main/java/com/kamikazejam/kamicommon/command/internal/CmdOpenGUI.java
@@ -6,9 +6,13 @@ import com.kamikazejam.kamicommon.command.requirement.RequirementHasPerm;
 import com.kamikazejam.kamicommon.command.requirement.RequirementIsPlayer;
 import com.kamikazejam.kamicommon.command.type.primitive.TypeString;
 import com.kamikazejam.kamicommon.gui.container.KamiMenuContainer;
+import com.kamikazejam.kamicommon.item.IBuilder;
+import com.kamikazejam.kamicommon.item.ItemBuilder;
 import com.kamikazejam.kamicommon.util.StringUtil;
 import com.kamikazejam.kamicommon.util.exception.KamiCommonException;
+import com.kamikazejam.kamicommon.xseries.XMaterial;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class CmdOpenGUI extends KamiCommand {
@@ -25,6 +29,11 @@ public class CmdOpenGUI extends KamiCommand {
     public void perform() throws KamiCommonException {
         Player player = (Player) sender;
         String guiKey = readArg();
+        if (guiKey.equalsIgnoreCase("paged")) {
+            this.openPaged(player);
+            return;
+        }
+
         KamiMenuContainer menu = new KamiMenuContainer(PluginSource.getKamiConfig(), guiKey);
         for (String key : menu.getIconKeys()) {
             menu.setMenuClick(key, (p, c) ->
@@ -35,5 +44,19 @@ public class CmdOpenGUI extends KamiCommand {
                     p.sendMessage(StringUtil.t("&7Page Item Click (&f" + c.name() + "&7) on &f" + key)));
         }
         menu.openMenu(player);
+    }
+
+    private void openPaged(@NotNull Player player) {
+        KamiMenuContainer container = new KamiMenuContainer("&8&lSample Paged Menu", 5).setOrdered(true);
+        for (int i = 1; i < 45; i++) {
+            String key = "Item" + i;
+            IBuilder builder = new ItemBuilder(XMaterial.STONE)
+                    .setName("&fItem &7#" + i);
+            container.addPagedIcon(key, builder);
+            container.setMenuClick(key, (p, c) -> {
+                p.sendMessage(StringUtil.t("&7Page Item Click (&f" + c.name() + "&7) on &f" + key));
+            });
+        }
+        container.setFillerItem(new ItemBuilder(XMaterial.RED_STAINED_GLASS_PANE).setName("&7")).openMenu(player);
     }
 }
