58fa4a4f6bf9da2e887b07a1343cec3f508f1200
Jake Moore <jakealexandermoore@icloud.com>
2024-09-06 18:15:47 -0700

    3.6.0.5 -Improved CombatIntegration api
    Took 4 minutes

--- spigot-utils/src/main/java/com/kamikazejam/kamicommon/integrations/combat/DeluxeCombatImpl.java
+++ spigot-utils/src/main/java/com/kamikazejam/kamicommon/integrations/combat/DeluxeCombatImpl.java
@@ -2,21 +2,43 @@ package com.kamikazejam.kamicommon.integrations.combat;
 
 import nl.marido.deluxecombat.DeluxeCombat;
 import nl.marido.deluxecombat.objects.PVPPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.util.UUID;
 
 public class DeluxeCombatImpl implements CombatIntegration {
     public DeluxeCombatImpl() {}
 
     @Override
-    public boolean isTagged(Player player) {
+    public boolean isTagged(@NotNull Player player) {
         return DeluxeCombat.getAPI().isInCombat(player);
     }
 
     @Override
-    public void tag(Player victim, @Nonnull Player attacker) {
+    public boolean isTagged(@NotNull UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+        if (player == null) { return false; }
+        return DeluxeCombat.getAPI().isInCombat(player);
+    }
+
+    @Override
+    public void tag(@NotNull Player victim, @Nonnull Player attacker) {
         PVPPlayer pvpPlayer = DeluxeCombat.getInstance().getSettings().getPlayer(victim);
         pvpPlayer.getTagger().activate(attacker);
+    }
+
+    @Override
+    public void untag(@NotNull Player player) {
+        DeluxeCombat.getAPI().untag(player);
+    }
+
+    @Override
+    public void untag(@NotNull UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+        if (player == null) { return; }
+        DeluxeCombat.getAPI().untag(player);
     }
 }
