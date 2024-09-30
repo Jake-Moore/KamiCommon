f6fad269a222502f5e759b267a94174aead8a961
Jake Moore <jakealexandermoore@icloud.com>
2024-09-29 22:33:39 -0700

    3.6.1.1 -Update KamiCommonNMS
    Took 1 minute

--- spigot-utils/build.gradle.kts
+++ spigot-utils/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     // Add NMS library from KamiCommonNMS
-    api("com.kamikazejam.kamicommon:spigot-nms:1.0.7")
+    api("com.kamikazejam.kamicommon:spigot-nms:1.0.8")
     api(project(":standalone-utils"))
     api(project(":shared-utils"))
 
