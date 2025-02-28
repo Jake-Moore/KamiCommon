48b67ab19a614611e0be3ec3c9fdb9ee4e52dda2
Jake-Moore <jakealexandermoore@icloud.com>
2025-02-27 20:21:46 -0800

    4.0.0-beta.15 - Fix how CachedModuleConfig registers itself, since we often call registerObserver during Module init and when the ModuleConfig isn't ready   - New implementation registers the observer with the module, and uses the module as the observer adding additional manual triggers from onConfigLoaded in Module
    Took 11 minutes

--- spigot-utils/src/main/java/com/kamikazejam/kamicommon/configuration/spigot/CachedConfig.java
+++ spigot-utils/src/main/java/com/kamikazejam/kamicommon/configuration/spigot/CachedConfig.java
@@ -1,17 +1,11 @@
 package com.kamikazejam.kamicommon.configuration.spigot;
 
-import com.cryptomorin.xseries.XMaterial;
-import com.kamikazejam.kamicommon.modules.ModuleConfig;
 import com.kamikazejam.kamicommon.util.MessageBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-import java.util.Optional;
 
 @Getter @Setter
 @SuppressWarnings("unused")
@@ -31,11 +25,6 @@ public abstract class CachedConfig<T extends KamiConfig> implements ICachedConfi
         config.registerObserver(this);
     }
 
-    /**
-     * Manual reload method. Call this to have {@link #onConfigLoaded(KamiConfig)} called again & your cache reloaded.<br>
-     * This {@link CachedConfig} class as a member of {@link ConfigObserver} is automatically registered to receive all config reloads.<br>
-     * As such, calling this method is optional and not required. Any call to the config passed in the constructor, will trigger {@link #loadConfig}
-     */
     @Override
     public final void reloadConfig() {
         try {
@@ -64,23 +53,6 @@ public abstract class CachedConfig<T extends KamiConfig> implements ICachedConfi
     // ------------------------------------ //
     // Helper Methods
     // ------------------------------------ //
-
-    public void warn(@NotNull String message) {
-        Bukkit.getLogger().warning(message);
-    }
-
-    public final @NotNull List<XMaterial> loadMaterials(@NotNull ModuleConfig config, @NotNull String key) {
-        return config.getStringList(key).stream()
-                .map(XMaterial::matchXMaterial)
-                .filter(mat -> {
-                    boolean present = mat.isPresent();
-                    if (!present) { warn("Invalid material in '" + key + "': " + mat); }
-                    return present;
-                })
-                .map(Optional::get)
-                .toList();
-    }
-
     /**
      * Constructs a new {@link MessageBuilder} with the given key and this config.
      */
