85e844c34e263c7074b58ae59e764f93cbc62c0d
Jake-Moore <jakealexandermoore@icloud.com>
2025-08-21 19:39:36 -0700

    5.0.0-alpha.7 - Removed Jackson entirely   - no longer shaded or available as part of KamiCommon (to continue using Jackson, your own dependency will be necessary) - Updated Redis system to use plain Java String instead of custom classes   - Rather than using Jackson internally, users can decide externally what serialization method they prefer, and transform the String object themselves.
    Took 7 minutes

--- shared-jar/src/main/java/com/kamikazejam/kamicommon/redis/callback/RedisChannelCallback.java
+++ shared-jar/src/main/java/com/kamikazejam/kamicommon/redis/callback/RedisChannelCallback.java
@@ -2,9 +2,10 @@ package com.kamikazejam.kamicommon.redis.callback;
 
 import org.jetbrains.annotations.NotNull;
 
-public interface RedisChannelCallback<T> {
+public interface RedisChannelCallback {
     /**
      * @param channel The channel the message was received on
+     * @param message The raw string message received by the channel
      */
-    void onMessage(String channel, @NotNull T message);
+    void onMessage(String channel, @NotNull String message);
 }
