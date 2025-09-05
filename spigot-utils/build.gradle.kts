7c08443dd1ed9da22a9e7e069de134d9f35ebefb
Jake-Moore <jakealexandermoore@icloud.com>
2025-09-04 23:45:33 -0700

    5.0.0-alpha.25-SNAPSHOT - Add MiniMessageBuilder.java - Update KamiCommonNMS with MiniMessage support: `NmsAPI.getVersionedComponentSerializer()`
    Took 1 hour 36 minutes

--- spigot-utils/build.gradle.kts
+++ spigot-utils/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     // Add NMS library from KamiCommonNMS
-    api("com.kamikazejam.kamicommon:spigot-nms:1.2.0")
+    api("com.kamikazejam.kamicommon:spigot-nms:1.2.1-20250905.063921-7")
     api(project(":standalone-utils")) // Also includes shared-utils
 
     api("com.google.code.gson:gson:2.13.1")
