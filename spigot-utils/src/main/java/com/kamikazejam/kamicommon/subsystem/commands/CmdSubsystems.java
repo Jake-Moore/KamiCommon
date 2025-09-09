cee14fc08c494a5fd467d32f42aad15703cc1c11
Jake-Moore <jakealexandermoore@icloud.com>
2025-09-08 20:45:40 -0700

    5.0.0-alpha.26-SNAPSHOT - Extract common logic from CmdModules.java into CmdSubsystems.java   - Then added CmdFeatures.java
    Took 20 minutes

---
+++ spigot-utils/src/main/java/com/kamikazejam/kamicommon/subsystem/commands/CmdSubsystems.java
@@ -0,0 +1,84 @@
+package com.kamikazejam.kamicommon.subsystem.commands;
+
+import com.kamikazejam.kamicommon.KamiPlugin;
+import com.kamikazejam.kamicommon.command.CommandContext;
+import com.kamikazejam.kamicommon.command.KamiCommand;
+import com.kamikazejam.kamicommon.command.Parameter;
+import com.kamikazejam.kamicommon.command.requirement.RequirementHasPerm;
+import com.kamikazejam.kamicommon.command.type.primitive.TypeInteger;
+import com.kamikazejam.kamicommon.command.util.CommandPaging;
+import com.kamikazejam.kamicommon.nms.NmsAPI;
+import com.kamikazejam.kamicommon.nms.abstraction.chat.KMessage;
+import com.kamikazejam.kamicommon.nms.abstraction.chat.impl.KMessageSingle;
+import com.kamikazejam.kamicommon.subsystem.AbstractSubsystem;
+import com.kamikazejam.kamicommon.util.StringUtil;
+import com.kamikazejam.kamicommon.util.exception.KamiCommonException;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+@SuppressWarnings({"unused"})
+public class CmdSubsystems extends KamiCommand {
+    private final @NotNull KamiPlugin plugin;
+    private final @NotNull Function<KamiPlugin, List<AbstractSubsystem<?,?>>> supplier;
+    private final @NotNull String paginationTitle;
+    public CmdSubsystems(
+            @NotNull KamiPlugin plugin,
+            @Nullable String permission,
+            @NotNull String desc,
+            @NotNull Function<KamiPlugin, List<AbstractSubsystem<?,?>>> supplier,
+            @NotNull String paginationTitle,
+            @NotNull String... aliases
+    ) {
+        this.plugin = plugin;
+        this.supplier = supplier;
+        this.paginationTitle = paginationTitle;
+        addAliases(aliases);
+
+        if (permission != null) {
+            addRequirements(RequirementHasPerm.get(permission));
+        }
+
+        // Add page param
+        this.addParameter(Parameter.of(TypeInteger.get()).name("page").defaultValue(1));
+
+        setDesc(desc);
+    }
+
+    @Override
+    public void perform(@NotNull CommandContext context) throws KamiCommonException {
+        int page = readArg();
+        CommandSender sender = context.getSender();
+
+        // Loop through subsystems in alphabetical order
+        List<AbstractSubsystem<?,?>> subsystems = new ArrayList<>(supplier.apply(this.plugin));
+        subsystems.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+
+        // Create each line of the subsystem list
+        List<KMessageSingle> lines = new ArrayList<>();
+        int size = subsystems.size();
+        for (int i = 0; i < size; i++) {
+            AbstractSubsystem<?,?> subsystem = subsystems.get(i);
+            int pos = i + 1;
+            String status = subsystem.isEnabled() ? (subsystem.isSuccessfullyEnabled() ? "&aENABLED" : "&cERROR") : "&6DISABLED";
+            String content = " &7- &f" + getPaddedNumber(pos, size) + ". " + subsystem.getName() + " &7- " + status;
+            lines.add(new KMessageSingle(StringUtil.t(content)));
+        }
+
+        // Create the Pagination of Each Subsystem Line
+        List<KMessage> messages = CommandPaging.getPage(this, lines, page, paginationTitle);
+        NmsAPI.getMessageManager().processAndSend(sender, messages);
+    }
+
+    private @NotNull String getPaddedNumber(int value, int maxValue) {
+        // Count how many characters the max value has, so we can pad the current value accordingly
+        int maxChars = String.valueOf(maxValue).length();
+        String strValue = String.valueOf(value);
+        // Pad with spaces in front to match the length of maxChars
+        return " ".repeat(Math.max(0, maxChars - strValue.length())) + strValue;
+    }
+}
--- spigot-utils/src/main/java/com/kamikazejam/kamicommon/subsystem/module/commands/CmdModules.java
+++ spigot-utils/src/main/java/com/kamikazejam/kamicommon/subsystem/module/commands/CmdModules.java
@@ -1,56 +1,31 @@
 package com.kamikazejam.kamicommon.subsystem.module.commands;
 
 import com.kamikazejam.kamicommon.KamiPlugin;
-import com.kamikazejam.kamicommon.command.CommandContext;
 import com.kamikazejam.kamicommon.command.KamiCommand;
-import com.kamikazejam.kamicommon.command.requirement.RequirementHasPerm;
-import com.kamikazejam.kamicommon.subsystem.module.Module;
-import com.kamikazejam.kamicommon.util.StringUtil;
-import com.kamikazejam.kamicommon.util.exception.KamiCommonException;
-import org.bukkit.command.CommandSender;
+import com.kamikazejam.kamicommon.subsystem.AbstractSubsystem;
+import com.kamikazejam.kamicommon.subsystem.commands.CmdSubsystems;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * KamiCommand implementation that lists all modules in the provided {@link KamiPlugin} (see constructor).<br>
  * Construct an instance of this class and register it under your own {@link KamiCommand} class.
  */
 @SuppressWarnings("unused")
-public class CmdModules extends KamiCommand {
-    private final KamiPlugin plugin;
-    public CmdModules(KamiPlugin plugin, String permission) {
-        this.plugin = plugin;
-        addAliases("modules");
-
-        addRequirements(RequirementHasPerm.get(permission));
-
-        setDesc("List all modules");
+public class CmdModules extends CmdSubsystems {
+    public CmdModules(@NotNull KamiPlugin plugin, @Nullable String permission) {
+        this(plugin, permission, "modules");
     }
 
-    @Override
-    public void perform(@NotNull CommandContext context) throws KamiCommonException {
-        CommandSender sender = context.getSender();
-        sender.sendMessage(" ");
-        sender.sendMessage(StringUtil.t("&eModules:"));
-        // Loop through moduleList in alphabetical order
-        List<Module> moduleList = plugin.getModuleManager().getModuleList();
+    public CmdModules(@NotNull KamiPlugin plugin, @Nullable String permission, @NotNull String... aliases) {
+        super(plugin, permission,"List all modules", supplyModules(), "Modules", aliases);
+    }
 
-        List<Module> modules = new ArrayList<>(moduleList);
-        modules.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
-
-        for (Module module : modules) {
-            int p = moduleList.indexOf(module) + 1;
-            String pos = (p < 10 ? " " + p : String.valueOf(p));
-
-            if (module.isEnabled() && module.isSuccessfullyEnabled()) {
-                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &a" + String.valueOf(module.isEnabled()).toUpperCase()));
-            }else if (!module.isSuccessfullyEnabled()) {
-                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &c" + String.valueOf(module.isEnabled()).toUpperCase()));
-            }else {
-                sender.sendMessage(StringUtil.t("  &e" + pos + ". " + module.getName() + " &7- &6Disabled"));
-            }
-        }
+    private static @NotNull Function<KamiPlugin, List<AbstractSubsystem<?,?>>> supplyModules() {
+        return (plugin) -> new ArrayList<>(plugin.getModuleManager().getModuleList());
     }
 }
